0ad70d968e0c02d9b4fa7d0e0be52e03659bc6b1
Leo Lutz <leo@leolutz.com>
2026-02-13 18:17:04 -0700

    Add macro support and traits with attributes

--- src/Cartography/RiverElement.php
+++ src/Cartography/RiverElement.php
@@ -3,15 +3,31 @@
 namespace LsvEu\Rivers\Cartography;
 
 use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Support\Traits\Macroable;
 use Symfony\Component\Uid\Ulid;
 
 abstract class RiverElement implements Arrayable
 {
+    use Macroable;
+
     public string $id;
+
+    /**
+     * @var \ReflectionClass[]
+     */
+    protected array $_traits;
 
     public function __construct(array $attributes = [])
     {
+        $this->_traits = (new \ReflectionClass($this))->getTraits();
+
         $this->id = $attributes['id'] ?? Ulid::generate();
+
+        foreach ($this->_traits as $trait) {
+            if ($trait->hasMethod("hydrate{$trait->getShortName()}")) {
+                call_user_func([$this, "hydrate{$trait->getShortName()}"], $attributes);
+            }
+        }
     }
 
     public function getAllRiverElements(): array
@@ -21,9 +37,19 @@ abstract class RiverElement implements Arrayable
 
     public function toArray(): array
     {
-        return [
-            'id' => $this->id,
-        ];
+        return array_reduce(
+            $this->_traits,
+            function (array $carry, \ReflectionClass $trait) {
+                if ($trait->hasMethod("toArray{$trait->getShortName()}")) {
+                    return $carry + call_user_func([$this, "toArray{$trait->getShortName()}"]);
+                }
+
+                return $carry;
+            },
+            [
+                'id' => $this->id,
+            ],
+        );
     }
 
     public function validate(RiverMap $map): ?array
